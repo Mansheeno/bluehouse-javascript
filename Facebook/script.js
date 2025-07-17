df88b0e60e0120eec595fe0552d88fb7f2244cc2
Mansheeno <mansheeno@gmail.com>
2025-07-17 12:55:05 +0100

    update

---
+++ Facebook/script.js
@@ -0,0 +1,73 @@
+var database = [
+    {
+        username: "salisu",
+        password: "adamu"
+    },
+    {
+        username: "mansh",
+        password: "123",
+    },
+    {
+        username: "daniel",
+        password: "777",
+    },
+    {
+        username: "sarah",
+        password: "samuel",
+    },
+    {
+        username: "bello",
+        password: "999",
+    }
+]; 
+
+var newsfeed = [
+    {
+        username: "Tboy",
+        timeline: "Javascript is hard"
+    },
+    {
+        username: "salisu",
+        timeline: "Today is a good day"
+    },
+    {
+        username: "mary",
+        timeline: "Good afternoon everyone"
+    },
+    {
+        username: "daniel",
+        timeline: "Javascript is hard"
+    },
+    {
+        username: "sarah",
+        timeline: "Today is a good day"
+    },
+    {
+        username: "bello",
+        timeline: "Good afternoon everyone"
+    }
+]
+
+function isUserValid(username, password) {
+    for (var i = 0; i < database.length; i++) {
+        if (database[i].username === username &&
+            database[i].password === password) {
+                return true;
+            }
+    }
+    return false;
+}
+
+function signIn(username, password){
+    if (isUserValid(username, password)) {
+        console.log(newsfeed);
+} else {
+    alert("Wrong User Details!");
+}
+}
+
+var userNamePrompt = prompt("What is your username:");
+var passwordPrompt = prompt("What is your password:");
+
+
+signIn(userNamePrompt, passwordPrompt);
